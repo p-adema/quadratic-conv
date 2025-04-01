1f65055c9007274e7a9aca944c4872b6dbcd9948
p-adema <117938385+p-adema@users.noreply.github.com>
2025-04-01 10:21:55 +0200

    (code) fix pytorch version

--- src/kernels/__init__.py
+++ src/kernels/__init__.py
@@ -1,6 +1,6 @@
-from gaussian import GaussKernelIso2D, GaussKernelMulti2D
-from quadratic import QuadraticKernelMulti2D
-from utils import LearnedKernel
+from .gaussian import GaussKernelIso2D, GaussKernelMulti2D
+from .quadratic import QuadraticKernelMulti2D
+from .utils import LearnedKernel
 
 __all__ = [
     "GaussKernelIso2D",
